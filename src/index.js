e3961b4312eb723adca25cb1a79fd51b8d928e7c
Rachel Isaac <rachelisaac995@gmail.com>
2023-02-01 11:54:48 +0400

    Add event listeners to call the functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import './style.css';
+import "./style.css";
+import newGame from "./modules/newGame.js";
+import getUserScore from "./modules/formSubmit.js";
+
+const formSubmitBtn = document.querySelector("#form-submit-btn");
+
+newGame();
+
+formSubmitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  getUserScore();
+});
