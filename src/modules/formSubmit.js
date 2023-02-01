2e6361279c092ecc56bb594a39095f71420a26a6
Rachel Isaac <rachelisaac995@gmail.com>
2023-02-01 11:54:23 +0400

    Create function to post new scores to new game API

---
+++ src/modules/formSubmit.js
@@ -0,0 +1,26 @@
+// ("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/QTfdB0h9KWr1chJOORpg/scores/");
+const getUserScore = async () => {
+  const nameInputValue = document.querySelector("#name").value;
+  const scoreInputValue = document.querySelector("#score").value;
+  if (!nameInputValue && !scoreInputValue) {
+    alert("Please fill all input fields");
+    return;
+  }
+  const response = await fetch(
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/QTfdB0h9KWr1chJOORpg/scores/",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        user: document.querySelector("#name").value,
+        score: document.querySelector("#score").value,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }
+  );
+  const data = await response.json();
+  console.log(data);
+};
+
+export default getUserScore;
