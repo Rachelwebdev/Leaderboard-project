938851e478fd640b04589adac8bbc1eb62ed5da9
Rachel Isaac <rachelisaac995@gmail.com>
2023-02-01 16:59:23 +0400

    Created a new ID for API url

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import './style.css';
-import postUserScore from './modules/formSubmit.js';
-import renderUserScores from './modules/renderScore.js';
-import getUserScore from './modules/refresh.js';
+import "./style.css";
+import postUserScore from "./modules/formSubmit.js";
+import renderUserScores from "./modules/renderScore.js";
+import getUserScore from "./modules/refresh.js";
 
-const nameInputField = document.querySelector('#name');
-const scoreInputField = document.querySelector('#score');
-const formSubmitBtn = document.querySelector('#form-submit-btn');
-const refreshButton = document.getElementById('refresh-btn');
+const nameInputField = document.querySelector("#name");
+const scoreInputField = document.querySelector("#score");
+const formSubmitBtn = document.querySelector("#form-submit-btn");
+const refreshButton = document.getElementById("refresh-btn");
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   renderUserScores();
 });
 
-formSubmitBtn.addEventListener('click', (event) => {
+formSubmitBtn.addEventListener("click", (event) => {
   event.preventDefault();
   postUserScore();
-  nameInputField.value = '';
-  scoreInputField.value = '';
+  nameInputField.value = "";
+  scoreInputField.value = "";
 });
 
-refreshButton.addEventListener('click', (e) => {
+refreshButton.addEventListener("click", (e) => {
   e.preventDefault();
   getUserScore();
   renderUserScores();
--- src/modules/formSubmit.js
+++ src/modules/formSubmit.js
@@ -1,22 +1,22 @@
 const postUserScore = async () => {
-  const nameInputValue = document.querySelector('#name').value;
-  const scoreInputValue = document.querySelector('#score').value;
-  const alert = document.querySelector('.alert');
+  const nameInputValue = document.querySelector("#name").value;
+  const scoreInputValue = document.querySelector("#score").value;
+  const alert = document.querySelector(".alert");
   if (!nameInputValue && !scoreInputValue) {
-    alert.innerHTML = 'Please fill all input fields';
+    alert.innerHTML = "Please fill all input fields";
   }
   const response = await fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/QTfdB0h9KWr1chJOORpg/scores/',
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/FhyouDxphP2Kp8D4e3Jj/scores/",
     {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({
-        user: document.querySelector('#name').value,
-        score: document.querySelector('#score').value,
+        user: document.querySelector("#name").value,
+        score: document.querySelector("#score").value,
       }),
       headers: {
-        'Content-type': 'application/json; charset=UTF-8',
+        "Content-type": "application/json; charset=UTF-8",
       },
-    },
+    }
   );
   const scoreDataPosted = await response.json();
   return scoreDataPosted;
