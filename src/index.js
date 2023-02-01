ed36c5e81e3b92b4ff027cbb4be036547968db60
Rachel Isaac <rachelisaac995@gmail.com>
2023-02-01 14:02:17 +0400

    Add event listener to refresh button

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import "./style.css";
 import newGame from "./modules/newGame.js";
-import getUserScore from "./modules/formSubmit.js";
+import postUserScore from "./modules/formSubmit.js";
+import renderUserScores from "./modules/renderScore";
 
 const formSubmitBtn = document.querySelector("#form-submit-btn");
+const refreshButton = document.getElementById("refresh-btn");
 
-newGame();
+window.addEventListener("load", () => {
+  newGame();
+});
 
 formSubmitBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  getUserScore();
+  postUserScore();
+});
+
+refreshButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  renderUserScores();
 });
